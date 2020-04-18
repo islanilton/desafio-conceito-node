ee1fcadd923e46affcd63a84f2e25794b0f8a86b
Islanilton <islanilton.rodrigues@gmail.com>
2020-04-18 09:26:41 -0300

    implemented the CRUD repository and lik creation features

--- src/app.js
+++ src/app.js
@@ -11,8 +11,23 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next) {
+  const {id} = request.params;
+  const indexRepository = repositories.findIndex(repository => repository.id === id);
+
+  if(indexRepository < 0) {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+
+  request.indexRepository = indexRepository;
+
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId);
+
 app.get("/repositories", (request, response) => {
-  
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
@@ -23,15 +38,39 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const {title, url, techs} = request.body;
+  const {indexRepository} = request;
+
+  const repository = {
+    ...repositories[indexRepository],
+    title,
+    url,
+    techs
+  };
+
+  repositories[indexRepository] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {indexRepository} = request;
+  repositories.splice(indexRepository, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {indexRepository} = request; 
+  const repository = {
+    ...repositories[indexRepository],
+    likes: repositories[indexRepository].likes + 1
+  };
+
+  repositories[indexRepository] = repository;
+
+  return response.json(repository);
 });
 
 module.exports = app;
