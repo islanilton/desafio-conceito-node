b5414997ceadc00dd15a3fb6a1141cac646d08a4
Islanilton <islanilton.rodrigues@gmail.com>
2020-04-17 21:38:45 -0300

    implemented add repository feature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {uuid} = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,11 +12,14 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repository = {id: uuid(), title, url, techs, likes: 0};
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
